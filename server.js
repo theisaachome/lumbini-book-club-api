55c8e0ce8aa7d6f5a6587eb95a383e54e711a75b
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-11 20:23:24 +0630

    add dotenv and database connection

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const app = express();
+const connectDB = require("./config/db");
+connectDB();
+const PORT = process.env.PORT ||8000;
+
+app.listen(PORT,()=>{
+    console.log(`Server started on ${PORT}`.green.underline.bold);
+})
