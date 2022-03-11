d76cbb5aaca5da864d9e140b1014a3aa0c5908ec
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-11 20:44:37 +0630

    book routes setup

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,4 +1,10 @@
 
 const express = require("express");
+const { getAllBooks, createBook, getBook, updateBook, deleteBook } = require("../controllers/bookController");
 const router = express.Router();
 
+
+router.route("/").get(getAllBooks).post(createBook);
+router.route("/:id").get(getBook).put(updateBook).delete(deleteBook);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const connectDB = require("./config/db");
 connectDB();
 const PORT = process.env.PORT ||8000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// Mount routes files
+const books = require('./routes/bookRoutes');
+
+app.use("/api/v1/books",books);
+
+
 app.listen(PORT,()=>{
     console.log(`Server started on ${PORT}`.green.underline.bold);
 });
