2ddfb2d4c6b361f622d0b8ccd9d70bdbb2533812
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-22 02:43:29 +0630

    setup email sender

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,10 +72,14 @@ exports.forgotPassword = asyncHandler(async(req,res,next)=>{
 
     await account.save({validateBeforeSave:false});
     
-    console.log(resetToken);
-    res.status(200).json(
-        {status:true,account}
-    )
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host',
+  )}/api/v1/auth/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  
 })
 
 
