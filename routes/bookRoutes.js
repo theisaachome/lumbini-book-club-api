85d8a5ef67ef8c3ec83f23099adf1fa510d345cc
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-25 16:35:39 +0630

    advancedResult middleware

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -8,11 +8,17 @@ const {
     deleteBook } = require("../controllers/bookController");
 const { requiredSignin } = require("../middlewares/authMiddleware");
 const bookReviewRoutes = require('./bookreviewRoutes');
+
+const advancedResults = require('../middlewares/advancedResult');
+const Book = require("../models/Book");
+
 const router = express.Router();
 
 router.use("/:bookid/bookreviews",bookReviewRoutes);
 
-router.route("/").get(getAllBooks).post(requiredSignin,createBook);
+router.route("/")
+    .get(advancedResults(Book),getAllBooks)
+    .post(requiredSignin,createBook);
 router.route("/:id").get(getBook).put(requiredSignin,updateBook).delete(requiredSignin,deleteBook);
 
 module.exports = router;
