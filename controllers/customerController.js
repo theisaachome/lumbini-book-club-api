ff338626dcebeb823e24b85eecbe0876652ac1b9
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-04-02 21:46:43 +0630

    customer resouces controller

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,18 +1,22 @@
 
 const asyncHandler = require('express-async-handler');
+const Customer = require('../models/Customer');
 
 exports.getAllCustomers = asyncHandler(async(req,res,next)=>{
-
+    const customers = await Customer.find();
+    res.send("Customer endpoints.");
 });
 
 
 
 exports.getCustomer = asyncHandler(async(req,res,next)=>{
-    
+    res.send("Customer endpoints.");
 });
 
 exports.updateCustomer = asyncHandler(async(req,res,next)=>{
-
+    res.send("Customer endpoints.");
 });
 
-exports.deleteCustomer = asyncHandler(async(req,res,next)=>{})
+exports.deleteCustomer = asyncHandler(async(req,res,next)=>{
+    res.send("Customer endpoints.");
+})
