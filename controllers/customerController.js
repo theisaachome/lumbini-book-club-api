2bb54b73b19498b773c70d23f654b66086e0427f
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-04-02 15:30:44 +0630

    customer controller

---
+++ controllers/customerController.js
@@ -0,0 +1,18 @@
+
+const asyncHandler = require('express-async-handler');
+
+exports.getAllCustomers = asyncHandler(async(req,res,next)=>{
+
+});
+
+
+
+exports.getCustomer = asyncHandler(async(req,res,next)=>{
+    
+});
+
+exports.updateCustomer = asyncHandler(async(req,res,next)=>{
+
+});
+
+exports.deleteCustomer = asyncHandler(async(req,res,next)=>{})
