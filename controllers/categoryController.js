f343c3aec00cfbb559c5e882e5da87b4c67c74bb
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-04-04 22:13:30 +0630

    get a single category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 const  asyncHandler= require('express-async-handler');
 const Category = require('../models/Category');
+const ErrorResponse = require('../utils/errorResponse');
 
 exports.getAllCategories = asyncHandler(async(req,res,next)=>{
     const categories = await Category.find().sort('name');
@@ -7,5 +8,15 @@ exports.getAllCategories = asyncHandler(async(req,res,next)=>{
         success:true,
         count:categories.length,
         data:categories,
+    });
+})
+
+
+exports.getCategory = asyncHandler(async(req,res,next)=>{
+    const category = await Category.findById(req.params.id);
+    if(!category)return next(new ErrorResponse(`There is no category with id ${req.params.id}`,404));
+    res.status(200).json({
+        success:true,
+        data:category,
     });
 })
