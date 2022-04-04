22b7d844e8ada0f0ca60824057a32b637707acbd
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-04-04 22:21:20 +0630

    add new category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -19,4 +19,17 @@ exports.getCategory = asyncHandler(async(req,res,next)=>{
         success:true,
         data:category,
     });
+});
+
+exports.addCategory = asyncHandler(async(req,res,next)=>{
+    const {name} = req.body.name;
+    if(!name)return next(new ErrorResponse(`Please add a name for category.`));
+
+    const category = await Category.create({name});
+    if(!category) return next(new ErrorResponse(`Category can not be created now.`,400));
+    res.status(201).json({
+        success:true,
+        data:category,
+    });
 })
+
