7f600455d4e1256cd01bd7a6b996a8ed80227a56
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-05-03 10:48:11 +0630

    api cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -9,6 +10,7 @@ connectDB();
 const PORT = process.env.PORT ||8000;
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
 
