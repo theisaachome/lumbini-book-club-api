de610d205af912696ac5665e983ec75980ee7e84
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-05-08 00:22:47 +0630

    token header update

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -12,6 +12,7 @@ exports.requiredSignin = asyncHandler(async (req, res, next) => {
         req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
     }
+    token = req.header('x-auth-token');
     if(!token){
         return next(new ErrorResponse('Not authorized to access this route', 401));
     }
